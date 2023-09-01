948ef80ab78b60fc08b134b36c747b338f98ba48
okewunmi <okewunmiafeezolaide@gmail.com>
2023-09-01 11:43:19 +0100

    styled-com

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/globalStyles.js";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#355C7D",
+  },
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
